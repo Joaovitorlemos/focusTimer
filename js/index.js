0146c2bdb22c05eaa1739388746062d3f91f0a54
Joaovitorlemos <joaovitorlemos2015@gmail.com>
2023-06-07 02:44:42 -0300

    adding elements.js to variables

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 import Controls from "./controls.js"
 import Timer from "./timer.js"
+import { elements } from "./elements.js"
 
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector('.stop')
-const buttonSet = document.querySelector('.set')
-const buttonSoundOn = document.querySelector('.sound-on')
-const buttonSoundOff = document.querySelector('.sound-off')
-const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds')
+const {
+   buttonPlay,
+   buttonPause,
+   buttonStop,
+   buttonSet,
+   buttonSoundOn,
+   buttonSoundOff,
+   minutesDisplay,
+   secondsDisplay
+} = elements
 
 const controls = Controls({
    buttonPlay,
